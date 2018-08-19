6588786845250a1b5dc792a619753e62530f5999
Riccardo Albertazzi <piano.vergato@gmail.com>
2018-08-19 20:54:14 +0200

    Added routing and navbar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ForecastComponent } from './forecast/forecast.component'
+ 
+const routes: Routes = [
+    { path: '', redirectTo: '/forecast', pathMatch:'full' },
+    { path: 'forecast', component: ForecastComponent }
+];
+ 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [ForecastComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ForecastComponent } from './forecast/forecast.component'
 import { FormsModule } from '@angular/forms'
-import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component'
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ForecastComponent
+    NavbarComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
